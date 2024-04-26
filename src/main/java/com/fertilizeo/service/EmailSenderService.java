d2edf4704e79b6af68f447faab5e0d65add96353
Princy159 <princyheryzo@gmail.com>
2024-04-26 13:39:32 +0300

    modification envoye email RGPD

--- src/main/java/com/fertilizeo/service/EmailSenderService.java
+++ src/main/java/com/fertilizeo/service/EmailSenderService.java
@@ -1,16 +1,18 @@
 package com.fertilizeo.service;
 
 import com.fertilizeo.config.jwt.JwtTokenValidationUtil;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
-
     @Autowired
     private JavaMailSender javaMailSender;
 
@@ -21,16 +23,34 @@ public class EmailSenderService {
     private String fromMail;
 
     @Async
-    public void sendEmail (String toEmail,
-                           String name){
+    public void sendEmail(String toEmail, String name) throws MessagingException {
         String token = jwtTokenValidationUtil.generateToken(toEmail);
         String link = "http://localhost:8080/compte/verify?token=" + token;
+        String termsLink = "https://docs.google.com/document/d/1WaqgRbOCfFSTOBH2Z0IchA3K81F2TpIcng-nn6Y-c6A/edit?usp=sharing";
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromMail);
-        message.setTo(toEmail);
-        message.setSubject("Confirmer votre addresse e-mail");
-        message.setText("Bienvenue Chez Fertilizeo " + name + ",\n\nVeuillez clicker sur ce lien pour verifier votre adresse e-mail:\n" + link);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(fromMail);
+        helper.setTo(toEmail);
+        helper.setSubject("Bienvenue chez Fertilizeo - Veuillez confirmer votre inscription");
+
+        String htmlMsg = "<h1>Bienvenue chez Fertilizeo, " + name + "!</h1>"
+                + "<p>Merci de vous joindre à nous. Veuillez confirmer votre adresse e-mail en cliquant sur le bouton ci-dessous:</p>"
+                + "<a href='" + link + "' style='padding: 10px; background-color: blue ; color: white; text-decoration: none; border-radius: 5px;'>Confirmer Email</a>"
+                + "<p>En confirmant votre adresse e-mail, vous consentez à l'utilisation de vos données personnelles conformément à notre <a href='" + termsLink + "' style='color: blue;'>Politique de Confidentialité</a>.</p>"
+                + "<p>Il est important de lire et comprendre notre politique de confidentialité pour être informé sur la manière dont nous utilisons vos données personnelles et les droits que vous avez à cet égard.</p>"
+                + "<p>Nous vous remercions pour votre confiance et nous nous engageons à protéger vos données personnelles.</p>"
+
+                + "<p>Cordialement, <br/> L'équipe Fertilizeo</p>";
+
+
+        helper.setText(htmlMsg, true);
+
         javaMailSender.send(message);
     }
 }
+
+
+
+
