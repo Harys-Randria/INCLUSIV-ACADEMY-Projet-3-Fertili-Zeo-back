baeca3d76e9fde3962ed39b92f0b0f2c68d6a0ad
Raivo <raivorakotonoely@gmail.com>
2024-04-28 12:43:24 +0300

    traitement image

--- src/main/java/com/fertilizeo/controller/ProductController.java
+++ src/main/java/com/fertilizeo/controller/ProductController.java
@@ -5,7 +5,9 @@ import com.fertilizeo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,7 +55,19 @@ import java.util.Optional;
             productService.deleteProduct(id);
             return ResponseEntity.ok().build();
         }
+
+        @GetMapping("/{produitId}/image-url")
+        public String getImageUrl(@PathVariable Long idproduit) {
+            return productService.getImageUrl(idproduit);
+        }
+
+        @PostMapping("/{produitId}/upload-image")
+        public void uploadImage(@PathVariable Long idproduit, @RequestParam("image") MultipartFile imageFile) throws IOException {
+            String resizedImageUrl = productService.resizeAndCompressImage(imageFile, 300, 300, 0.8f);
+            productService.saveImageUrl(idproduit, resizedImageUrl);
+        }
     }
+
 
 
 
